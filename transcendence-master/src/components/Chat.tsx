d42c63c0f8eae3b818a223883fd3eb85af961e6c
Ihssane Ouardi <iouardi@c3r2p3.1337.ma>
2023-08-22 19:14:54 +0100

    work in progress

--- transcendence-master/src/components/Chat.tsx
+++ transcendence-master/src/components/Chat.tsx
@@ -1,15 +1,73 @@
 // import  { Link } from 'react-router-dom';
+// import React from 'react';
+import logoImg from '../assets/panda.svg';
+
+
+
+interface ConvData  {
+    id: number;
+    name: string;
+    image: string;
+    message:  string;
+    date: string;
+    group: string;
+}
+
+const DMComp = () =>
+{
+  const defaultConvData: ConvData = {
+    id: 1,
+    name: 'Mohamed',
+    image: logoImg,
+    message: 'Hello everyone!',
+    date: 'Today, 12:15pm',
+    group: 'Friends Forever'
+  };
+  
+  return (
+    <div  className='icon w-[90px] h-[40px] ml-[-120px] mb-[15px]'>
+      <img  className='logoImg rounded-[50px] w-[40px] h-[40px]' src={defaultConvData.image} alt={'${defaultConvdata.name}'} />
+  
+      <div className='groupName mb-[40px] text-black dark:text-white w-[150px] mt-[-40px] ml-[45px]' style={{ fontFamily: 'Roboto', fontSize: '15px',fontStyle: 'normal', fontWeight: 600, lineHeight: 'normal', letterSpacing: '0.75px'}}>
+        {defaultConvData.group}
+      </div>
+      <div className='groupMsg text-black dark:text-white w-[150px] mt-[-40px] ml-[45px]' style={{ fontFamily: 'Roboto', fontSize: '13px',fontStyle: 'normal', fontWeight: 300, lineHeight: 'normal', letterSpacing: '0.65px'}} >
+        {defaultConvData.message}
+      </div>
+      <div className='date w-[90px] mt-[-30px] ml-[180px]' style={{color: '#7C7C7C', fontFamily: 'Roboto', fontSize: '13px', fontStyle: 'normal', fontWeight: 300, lineHeight: 'normal', letterSpacing: '0.13px' }}>
+          {defaultConvData.date}
+      </div>
+  
+    </div>
+  )
+}
 
 const Chat = () => {
   return (
-    <div className="chat ml-[2px] mr-[25px] mb-[15px] h-full w-full flex flex-wrap 100vh gap-[0px]">
-      <div className="ml-[25px] mr-[1px] my-[15px] h-full w-full flex-1 flex flex-wrap gap-[0px] items-start">
-        <div className="w-[90%] h-[32%] rounded-[25px] border-solid flex flex-3 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+    <div className="chat lg:ml-[2px] lg:mr-[2px] lg:mb-[10px] lg:h-full lg:w-full lg:flex lg:flex-wrap lg:100vh lg:gap-[0px] 
+    ml-[2px] mr-[2px] mb-[10px] h-full w-full flex flex-wrap 100vh gap-[0px]">
+      <div className="lg:ml-[20px] lg:mr-[-10px] lg:my-[15px] lg:h-full lg:w-full lg:flex-1 lg:flex lg:flex-wrap lg:gap-[0px] lg:items-start
+      ml-[20px] mr-[-10px] my-[15px] h-full w-full flex-1 flex flex-wrap gap-[0px] items-start">
+        <div className="lg:w-[90%] lg:h-[32%] lg:rounded-[25px] lg:border-solid lg:flex lg:flex-row lg:flex-3 lg:border-[#FFFFFF] lg:bg-[#FFFFFF]  lg:shadow-none lg:dark:border-[#272932] lg:dark:bg-[#272932]
+        w-[90%] h-[32%] rounded-[25px] border-solid flex flex-3 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+          <div className="group ml-[25px] text-black dark:text-white" style={{fontFamily: 'Roboto', fontSize: '30px', fontStyle: 'normal', fontWeight: 700, letterSpacing: '1.5px' }}>Groups
+          </div>
+          <div className='convs mt-[70px]'>
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+          </div>
         </div>
-        <div className="mt-[-99px] w-[90%] h-[55%] rounded-[25px]  border-solid flex flex-4 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+        <div className="lg:mb-[5px] lg:mt-[-97px] lg:w-[90%] lg:h-[55%] lg:rounded-[25px]  lg:border-solid lg:flex lg:flex-4 lg:border-[#FFFFFF] lg:bg-[#FFFFFF]  lg:shadow-none lg:dark:border-[#272932] lg:dark:bg-[#272932]
+        mb-[5px] mt-[-97px] w-[90%] h-[55%] rounded-[25px]  border-solid flex flex-4 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+          <div className="dms ml-[25px] text-black dark:text-white" style={{fontFamily: 'Roboto', fontSize: '30px', fontStyle: 'normal', fontWeight: 700, letterSpacing: '1.5px' }}>People
+            
+          </div>
         </div>
       </div>
-      <div className="my-[15px] w-[70%] h-[88%] rounded-[25px] flex-2 flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+      <div className="lg:ml-[-10px] lg:mr-[15px] lg:my-[15px] lg:w-[70%] lg:h-[88%] lg:rounded-[25px] lg:flex-2 lg:flex-shrink-0 lg:border-solid lg:border-[#FFFFFF] lg:bg-[#FFFFFF]  lg:shadow-none lg:dark:border-[#272932] lg:dark:bg-[#272932]
+      ml-[-10px] mr-[15px] my-[15px] w-[50%] h-[88%] rounded-[25px] flex-2 flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
       </div>
     </div> 
   )
