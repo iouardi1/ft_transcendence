bc1829b0f72a8bfd7564f0d45e8748408e3ebc85
Ihssane Ouardi <iouardi@c3r2p3.1337.ma>
2023-08-22 12:53:26 +0100

    chat ui in progress

--- transcendence-master/src/components/Chat.tsx
+++ transcendence-master/src/components/Chat.tsx
@@ -2,14 +2,14 @@
 
 const Chat = () => {
   return (
-    <div className="chat ml-[25px] mr-[25px] h-full w-full flex grid-cols-2 justify-around">
-      <div className="mr-[20px] my-[15px] h-full w-full grid grid-row-2 divide-y-[300px]">
-        <div className="p-[10px] w-[90%] h-[70%] rounded-[25px] flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+    <div className="chat ml-[2px] mr-[25px] mb-[15px] h-full w-full flex flex-wrap 100vh gap-[0px]">
+      <div className="ml-[25px] mr-[1px] my-[15px] h-full w-full flex-1 flex flex-wrap gap-[0px] items-start">
+        <div className="w-[90%] h-[32%] rounded-[25px] border-solid flex flex-3 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
         </div>
-        <div className="w-[90%] h-[80%] rounded-[25px] flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+        <div className="mt-[-99px] w-[90%] h-[55%] rounded-[25px]  border-solid flex flex-4 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
         </div>
       </div>
-      <div className="my-[15px] w-[70%] h-[87%] rounded-[25px] flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+      <div className="my-[15px] w-[70%] h-[88%] rounded-[25px] flex-2 flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
       </div>
     </div> 
   )
