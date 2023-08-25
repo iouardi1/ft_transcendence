e3c7548e9d2e21a5793cc54dd93a6ca5774d28f3
Ihssane Ouardi <iouardi@c3r2p2.1337.ma>
2023-08-25 14:12:27 +0100

    chat component in progress

--- transcendence-master/src/components/Chat.tsx
+++ transcendence-master/src/components/Chat.tsx
@@ -11,6 +11,7 @@ interface ConvData  {
     message:  string;
     date: string;
     group: string;
+    online: string;
 }
 
 const DMComp = () =>
@@ -21,11 +22,12 @@ const DMComp = () =>
     image: logoImg,
     message: 'Hello everyone!',
     date: 'Today, 12:15pm',
-    group: 'Friends Forever'
+    group: 'Friends Forever',
+    online: 'Online - Last seen, 2.02pm' 
   };
   
   return (
-    <div  className='icon w-[90px] h-[40px] ml-[-120px] mb-[15px]'>
+    <div  className='icon w-full h-[40px] ml-[-120px] mb-[15px]'>
       <img  className='logoImg rounded-[50px] w-[40px] h-[40px]' src={defaultConvData.image} alt={'${defaultConvdata.name}'} />
   
       <div className='groupName mb-[40px] text-black dark:text-white w-[150px] mt-[-40px] ml-[45px]' style={{ fontFamily: 'Roboto', fontSize: '15px',fontStyle: 'normal', fontWeight: 600, lineHeight: 'normal', letterSpacing: '0.75px'}}>
@@ -34,13 +36,49 @@ const DMComp = () =>
       <div className='groupMsg text-black dark:text-white w-[150px] mt-[-40px] ml-[45px]' style={{ fontFamily: 'Roboto', fontSize: '13px',fontStyle: 'normal', fontWeight: 300, lineHeight: 'normal', letterSpacing: '0.65px'}} >
         {defaultConvData.message}
       </div>
-      <div className='date w-[90px] mt-[-30px] ml-[180px]' style={{color: '#7C7C7C', fontFamily: 'Roboto', fontSize: '13px', fontStyle: 'normal', fontWeight: 300, lineHeight: 'normal', letterSpacing: '0.13px' }}>
+      <div className='date w-[90px] mt-[-30px] ml-[190px]' style={{color: '#7C7C7C', fontFamily: 'Roboto', fontSize: '13px', fontStyle: 'normal', fontWeight: 300, lineHeight: 'normal', letterSpacing: '0.13px' }}>
           {defaultConvData.date}
       </div>
+      <hr className=" w-[300px] h-[1px] my-[30px] bg-[#2C2C2CBD] opacity-[65%] border-0 rounded  dark:bg-[#474444bd]"></hr>
   
     </div>
-  )
-}
+  );
+};
+
+const ContactBar = () =>
+{
+  const defaultConvData: ConvData = {
+    id: 1,
+    name: 'Mohamed',
+    image: logoImg,
+    message: 'Hello everyone!',
+    date: 'Today, 12:15pm',
+    group: 'Friends Forever',
+    online: 'Online - Last seen, 2.02pm'
+  };
+  
+  return (
+    <div  className='w-full h-full p-[20px] flex'>
+      <div className='w-[100%] flex'>
+        <img  className='logoImg rounded-[50px] w-[40px] h-[40px]' 
+              src={defaultConvData.image} 
+              alt={'${defaultConvdata.name}'} />
+              <div className='w-[80%] h-full  ml-[25px] justify-left'>
+                <div className=' text-black dark:text-white w-full h-[50%]' style={{ fontFamily: 'Roboto', fontSize: '20px',fontStyle: 'normal', fontWeight: 600, lineHeight: 'normal', letterSpacing: '1.5px'}}>
+                  {defaultConvData.name}
+                  </div>
+                  <div className='w-full h-[50%] mt-[7px] text-black dark:text-white'   style={{ fontFamily: 'Roboto', fontSize: '15px',fontStyle: 'normal', fontWeight: 300, lineHeight: 'normal', letterSpacing: '1.5px'}}>
+                  {defaultConvData.online}
+                  </div>
+                  <hr className=" w-[90%] h-[1px] my-[15px] bg-[#2C2C2CBD] opacity-[65%] border-0 rounded  dark:bg-[#474444bd]"></hr>
+              </div>
+              
+      
+      </div>
+    </div>
+  );
+};
+
 
 const Chat = () => {
   return (
@@ -52,7 +90,7 @@ const Chat = () => {
         w-[90%] h-[32%] rounded-[25px] border-solid flex flex-3 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
           <div className="group ml-[25px] text-black dark:text-white" style={{fontFamily: 'Roboto', fontSize: '30px', fontStyle: 'normal', fontWeight: 700, letterSpacing: '1.5px' }}>Groups
           </div>
-          <div className='convs mt-[70px]'>
+          <div className='convs w-full h-full mt-[70px] align'>
             { < DMComp /> }
             { < DMComp /> }
             { < DMComp /> }
@@ -62,12 +100,24 @@ const Chat = () => {
         <div className="lg:mb-[5px] lg:mt-[-97px] lg:w-[90%] lg:h-[55%] lg:rounded-[25px]  lg:border-solid lg:flex lg:flex-4 lg:border-[#FFFFFF] lg:bg-[#FFFFFF]  lg:shadow-none lg:dark:border-[#272932] lg:dark:bg-[#272932]
         mb-[5px] mt-[-97px] w-[90%] h-[55%] rounded-[25px]  border-solid flex flex-4 border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
           <div className="dms ml-[25px] text-black dark:text-white" style={{fontFamily: 'Roboto', fontSize: '30px', fontStyle: 'normal', fontWeight: 700, letterSpacing: '1.5px' }}>People
-            
+            </div>
+            <div className='convs mt-[70px] ml-[10px]'>
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
+            { < DMComp /> }
           </div>
         </div>
       </div>
       <div className="lg:ml-[-10px] lg:mr-[15px] lg:my-[15px] lg:w-[70%] lg:h-[88%] lg:rounded-[25px] lg:flex-2 lg:flex-shrink-0 lg:border-solid lg:border-[#FFFFFF] lg:bg-[#FFFFFF]  lg:shadow-none lg:dark:border-[#272932] lg:dark:bg-[#272932]
-      ml-[-10px] mr-[15px] my-[15px] w-[50%] h-[88%] rounded-[25px] flex-2 flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none dark:border-[#272932] dark:bg-[#272932]">
+      ml-[-10px] mr-[15px] my-[15px] w-[50%] h-[88%] rounded-[25px] flex-2 flex-shrink-0 border-solid border-[#FFFFFF] bg-[#FFFFFF]  shadow-none flex flex-row-3 dark:border-[#272932] dark:bg-[#272932]">
+        <div className='w-full h-[10%] border-solid border-white'>
+          { <ContactBar />}
+        </div>
       </div>
     </div> 
   )
